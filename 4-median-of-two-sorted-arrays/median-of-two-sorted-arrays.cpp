8233ec04355b5e027baad9ba5b442dd0432b7e2e
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2023-09-06 00:29:34 +0530

    Time: 30 ms (57.32%) | Memory: 89.7 MB (33.76%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,14 +4,13 @@ public:
 
         nums2.insert(nums2.begin(),nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
+        
         if(nums2.size()&1){
-            // cout<<<<endl;
             return nums2[nums2.size()/2]; 
         }
         else{
             return (nums2[nums2.size()/2-1]+nums2[nums2.size()/2])/2.00;
         }
-        return 0.00;
 
     }
 };
