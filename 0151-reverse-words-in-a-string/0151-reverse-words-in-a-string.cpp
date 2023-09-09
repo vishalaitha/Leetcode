3b6d6761366f02d3faa9250b08041e13f909a733
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2023-09-09 13:05:08 +0530

    Time: 17 ms (8.05%), Space: 7.2 MB (90.04%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans;
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            cout<<i<<" ";
+            if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z'||s[i]>='0'&&s[i]<='9'){
+                int j=i;
+                string temp;
+                 while(j>=0&&s[j]!=' '){
+                     temp+=s[j];
+                     j--;
+                 }
+                //  cout<<temp<<endl;
+                 while(j>=0&&s[j]==' ') j--;
+                 reverse(temp.begin(),temp.end());
+                 ans+=temp;
+                 ans+=' ';
+                 i=j+1;
+            }
+        }
+        if(*ans.rbegin()==' ') ans.pop_back();
+        // cout<<ans<<endl;
+        return ans;
+    }
+};
